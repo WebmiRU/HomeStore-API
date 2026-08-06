c51ac7f7b347356011465f6f4afd5e0624eb98d5
E.Wolf <e.wolf@yudole.ru>
2026-08-06 17:05:40 +0300

    dev

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Ramsey\Uuid\Uuid;
 
 class CodeController extends Controller
 {
     public function index()
     {
-        return 'Code index';
+        return Uuid::uuid7()->toString();
     }
 
 
--- database/migrations/2026_08_06_134555_create_code_table.php
+++ database/migrations/2026_08_06_134555_create_code_table.php
@@ -13,7 +13,22 @@ return new class extends Migration
     {
         Schema::create('code', function (Blueprint $table) {
             $table->id();
+            $table->uuid('code')->unique();
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('store_id');
             $table->timestamps();
+
+            $table->foreign('item_id')
+                ->references('id')
+                ->on('item')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreign('store_id')
+                ->references('id')
+                ->on('store')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
         });
     }
 
